46e12f79b62a51d6be1d9197e6d2ee3a48146c29
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-07-27 15:40:12 +0530

    Update aruna.py

--- aruna.py
+++ aruna.py
@@ -2,10 +2,10 @@ n=int(raw_input())
 if(n>o):
   if(n%2==0):
     print("Even")
-    if(n%2>1):
-      print("Odd")
+  elif(n%2>1):
+    print("Odd")
 else:
-            print("invalid")
+  print("invalid")
 
 
 
