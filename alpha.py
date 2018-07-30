93c1e7f0efd21d9be8f6459ac097d2b41d3579ba
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-07-30 12:08:01 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,7 +1,7 @@
 print("enter '0' for exit")
-ch = input("enter any character: ")
+ch = input("enter any character: ");
 if ch=='0':
-  exit()
+  exit();
 else:
   if(ch>='a' and ch<='z'):
     print(ch,"is an alphabet")
