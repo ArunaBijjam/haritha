b1e7dee1df9366d8c558c1efdf1da13ab4fa5afa
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-07-30 12:15:41 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,9 +1,7 @@
-print("enter '0' for exit")
-ch = input("enter any character: ");
-if ch == '0':
-  exit();
+x=raw_input("")
+print(x)
+if(x>'a'):
+  print("alphabet")
 else:
-  if(ch>='a' and ch<='z'):
-    print(ch,"is an alphabet")
-  else:
-    print(ch,"not an alphabet")
+  print("not alphabet")
+  
