b59bf3371bdfc1b46fa69262ebc211fca3199e20
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-07-30 14:34:02 +0530

    Update large.py

--- large.py
+++ large.py
@@ -2,8 +2,8 @@ x=raw_input()
 y=raw_input()
 z=raw_input()
 if(x>y>z):
-print(x, "is larger")
+  print(x, "is larger")
 elif(y>x>z):
-print(y, "is larger")
+  print(y, "is larger")
 else:
-print(z, "is larger")
+  print(z, "is larger")
