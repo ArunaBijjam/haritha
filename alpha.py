fc12dafd20f4c1688244d4829a7516f74c029a4e
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-07-30 12:04:06 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,9 @@
+print("enter '0' for exit")
+ch = input("enter any character: ")
+if ch=='0':
+exit()
+else:
+if(ch>='a' and ch<='z'):
+print(ch,"is an alphabet")
+else:
+print(ch,"not an alphabet")
