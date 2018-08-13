8c972e4cbc869e79edf0b534647091a57e0f4f58
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-08-13 14:36:09 +0530

    Update armin.py

--- armin.py
+++ armin.py
@@ -1,10 +1,12 @@
-m,n=map(int,raw_input().split())
-for num in range (m,n):
-  sum = 0
-  temp = num
-  while temp > 0:
-    digit = temp % 10
-    sum += digit ** 3
-    temp //= 10
-  if num == sum:
-    print(num) 
+m=int(input())
+n=int(input())
+for num in range (m,n+1):
+	order=len(str(num))
+	sum = 0
+	temp = num
+	while temp > 0:
+		digit = temp % 10
+		sum += digit ** order
+		temp //= 10
+	if num == sum:
+		 print(num)# your code goes here
