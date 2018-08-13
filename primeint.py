9866e9e264f1a755d8b5763496832e8682c90542
ArunaBijjam <41769840+ArunaBijjam@users.noreply.github.com>
2018-08-13 13:18:32 +0530

    Update primeint.py

--- primeint.py
+++ primeint.py
@@ -1,9 +1,8 @@
-m=int(input())
-n=int(input())
-for num in range(m,n+1):
-	if num>1:
-		for i in range(2,num):
-			if(num%i)==0:
+m,n=map(int,raw_input().split())
+for i in range(m+1,n):
+	if(i>1):
+		for j in range(2,i):
+			if(i%j==0):
 				break
 		else:
-			print(num)
+			print i,
